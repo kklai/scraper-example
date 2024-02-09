90a29acea6257f2fe5fd61cf70c0b936fda9ed0a
Rebecca Lai <rebecca.lai@nytimes.com>
2024-02-09 09:11:04 -0800

    add comments

--- dl-html.js
+++ dl-html.js
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const cheerio = require('cheerio');
+const fs = require("fs"); // library used to read files on your computer
 
-let data = fs.readFileSync("data.csv", "utf8");
-let lines = data.split("\r\n")
+let data = fs.readFileSync("data.csv", "utf8"); // reads the data file
+let lines = data.split("\r\n") // split the csv into an array
 
-let out = "";
+let out = ""; // empty string for output
 
-lines.forEach(function(d,i){
+lines.forEach(function(d,i){ // for loop 
 
-    if ( i != 0) {
-        let split = d.split(",")
-        out += `wget "${split[3]}" -O html/a${split[0]}_${split[2].split(' ').join('-')}.html \n`
+    if ( i != 0) { // dont read the first line of the csv
+        let split = d.split(",") // split each line of the csv by comma
+        out += `wget "${split[3]}" -O html/a${split[0]}_${split[2].split(' ').join('-')}.html \n` // add a command for each line of the data that will download the html file with the wget library
     }
 })
 
-fs.writeFileSync("dl-html.sh", out)
+fs.writeFileSync("dl-html.sh", out) // write a shellfile for downloading
--- dl-images.js
+++ dl-images.js
@@ -1,27 +1,27 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const cheerio = require('cheerio');
+const fs = require("fs");  // library used to read files on your computer
+const cheerio = require('cheerio');  // library used to read parse html file
 
 let out = "";
-let files = fs.readdirSync("html");
+let files = fs.readdirSync("html"); // read all the files in the html folder
 
-files.forEach(function(d){
+files.forEach(function(d){ // loop through all the files in the html folder
 
-    let file = fs.readFileSync("html/" + d, "utf8")
+    let file = fs.readFileSync("html/" + d, "utf8") // read a single file
 
-    let $ = cheerio.load(file);
+    let $ = cheerio.load(file); // parse the file into html
 
-    let imgfile = $(".infobox .mw-file-description .mw-file-element")
+    let imgfile = $(".infobox .mw-file-description .mw-file-element") // select the element containing the movie poster, i figured this out by dragging the html file into the browser and inspecting element, took some trial an error
     
-    if (imgfile && imgfile.attr("src")) {
-        imgfile = imgfile.attr("src").replace('//upload', 'https://upload');
-        console.log(d, imgfile)
-        out += `wget "${imgfile}" -O img/${d.replace('.html', '.jpg')} \n`;
+    if (imgfile && imgfile.attr("src")) { // if it has the right element
+        imgfile = imgfile.attr("src").replace('//upload', 'https://upload'); // correct the url
+        console.log(d, imgfile) // log the output to make sure it looks right
+        out += `wget "${imgfile}" -O img/${d.replace('.html', '.jpg')} \n`; // create a wget command for each data entry
     } else {
-        console.log(d)
+        console.log(d) // if it failed then log the failure
     }
 
 })
 
-fs.writeFileSync("dl-images.sh", out)
+fs.writeFileSync("dl-images.sh", out) // write a shellfile for downloading
